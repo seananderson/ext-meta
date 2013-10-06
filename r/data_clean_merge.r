39a87d8b2532107a9744d7d93de399f6efa11f14
Sean Anderson <github@seananderson.ca>
2013-10-05 22:14:15 -0700

    Edit data_clean_merge.r to bring in new proxy data

--- r/data_clean_merge.r
+++ r/data_clean_merge.r
@@ -94,11 +94,23 @@ ext <- ext[, -grep("X.[0-9]+", names(ext))]
 stageIDX<-which(names(ext) %in% c("Start.stage", "End.stage"))
 
 #read in the data set with the conversions between stage and time
-#stageTime<-read.csv("../data/Stage vs time.csv")
+stageTime1<-read.csv("../data/Stage vs time.csv")
 stageTime <- read.csv("../data/stage-v-time-gradstein-2004-ts.csv")
 names(stageTime)[1] <- "End..Ma."
 names(stageTime)[2] <- "Start..Ma."
 names(stageTime)[3] <- "Bin.name"
+stageTime$Bin.name <- as.character(stageTime$Bin.name)
+# Fix some typos:
+stageTime$Bin.name[stageTime$Bin.name == "Olenikian"] <- "Olenekian"
+stageTime$Bin.name[stageTime$Bin.name == "Gzelian"] <- "Gzhelian"
+stageTime$Bin.name[stageTime$Bin.name == "Fammenian"] <- "Famennian"
+stageTime$Bin.name[stageTime$Bin.name == "Pridoli"] <- "Pridolian"
+stageTime$Bin.name[stageTime$Bin.name == "Sandbian"] <- "Sandblian" # replacing with typo
+stageTime$Bin.name[stageTime$Bin.name == "Darriwillian"] <- "Darriwilian"
+
+# add a "recent" row: (TODO someone should confirm this is OK)
+stageTime <- rbind(stageTime[1,], stageTime)
+stageTime[1, "Bin.name"] <- "Recent"
 
 #A function to take a lookup pair of stages and get the entire range of stages in between
 getStageRange<-function(startStage, endStage){
@@ -132,25 +144,25 @@ names(extMag2)[6]<-paste(names(extMag2)[6], "PBDB", sep=".")
 
 ######proxy data
 #proxy<-read.csv("../data/180 13C Sr 34S sealevel.csv")
-#proxy<-read.csv("../data/Hannisdal and Peters data with stage IDs.csv", na.strings="?")
+#proxy1<-read.csv("../data/Hannisdal and Peters data with stage IDs.csv", na.strings="?")
 
 proxy <- read.csv("../data/Prokoph_data_9.2013.csv")
 proxy <- subset(proxy, data_subset == "All")
-proxy <- rename(proxy, c("mean_d34S" = "del.34S", "Binned_top" = "top"))
+proxy <- rename(proxy, c("mean_d34S" = "del.34S", "Binned_top" = "top", "Binned_bottom" = "bottom"))
 proxy <- proxy[order(proxy$top), ]
 
-pdf("figure/detrending-plots.pdf")
+pdf("detrending-plots-prokoph.pdf")
 proxy$del.34S <- with(proxy, detrend_ts(top, del.34S, "Prokoph d34S"))
-
+proxy$mean_d18O <- with(proxy, detrend_ts(top, mean_d18O, "Prokoph d18O"))
+proxy$mean_d13C <- with(proxy, detrend_ts(top, mean_d13C, "Prokoph d13C"))
+dev.off()
 
 ######new sealevel data
 sealevel<-read.csv("../data/Sea level residuals after 2nd-order polynomial fit.csv")
 
-
-
 #Veizer delta 18O
-veizer<-read.csv("../data/veizer_d18O.csv")
-veizer$Length<-veizer$StageBottom-veizer$StageTop
+#veizer<-read.csv("../data/veizer_d18O.csv")
+#veizer$Length<-veizer$StageBottom-veizer$StageTop
 
 counter <<- 0
 
@@ -159,6 +171,7 @@ envtCols <- t(apply(ext[,stageIDX], 1, function(arow){
   counter <<- counter+1
   print(paste("row:",counter))
 
+  
   #check for NAs
   flag<-0
   if(sum(is.na(arow))>0){
@@ -198,16 +211,16 @@ envtCols <- t(apply(ext[,stageIDX], 1, function(arow){
 
 
   #get the row numbers from the veizer file
-  veizerIDX<-c(which(veizer$StageBottom >= times[1])[1], #first time within the stage
-              which(veizer$StageTop > times[2])[1]-1 #last time within the stage
-    )
-
+#  veizerIDX<-c(which(veizer$StageBottom >= times[1])[1], #first time within the stage
+#             which(veizer$StageTop > times[2])[1]-1 #last time within the stage
+#  )
+  
   #get the means for each column of the environmental data
   #rgh - kludge to turn a df into a vector, as colwise turns
   #out data frames
 #  envt<-t(colwise(mean)(proxy[proxyIDX,2:ncol(proxy)]))[,1] #old proxy data
-  envt<-t(colwise(function(x) mean(x, na.rm=T))(proxy[proxyIDX,13:17]))[,1]
-
+  #envt<-t(colwise(function(x) mean(x, na.rm=T))(proxy[proxyIDX,13:17]))[,1]
+  envt<-t(colwise(function(x) mean(x, na.rm=T))(proxy[proxyIDX,c(9,6,12)]))[,1]
 
   #query whether there were any events, and set the return value to 1 if so
   vb<-as.numeric(colSums(volcbolide[vbIDX,3:6])>0)
@@ -216,22 +229,22 @@ envtCols <- t(apply(ext[,stageIDX], 1, function(arow){
   ex<-c(BC.extinction.rate=mean(extMag$BC.extinction.rate[exIDX]))
   ex2<-c(BC.extinction.ratePBDB=mean(extMag2$BC.extinction.rate[exIDX2]))
 
-  vez<-t(colwise(function(x) mean(x, na.rm=T))(veizer[veizerIDX,4:10]))[,1]
+#  vez<-t(colwise(function(x) mean(x, na.rm=T))(veizer[veizerIDX,4:10]))[,1]
 #  vexWeight<-
-  vez<-with(veizer[veizerIDX,], {
-                      c(d18OresidualMean=mean(d18Oresidual),
-                        d18OresidualMeanWeighted=weighted.mean(d18Oresidual, Length),
-                        d18OresidualSD=sd(d18Oresidual),
-                        Dd18O.first.differences.Mean=mean(Dd18O.first.differences., na.rm=T),
-                      #  Dd18O.first.differences.MeanWeighted=weighted.mean(Dd18O.first.differences., length, na.rm=T),
-                        Dd18O.first.differences.SD=sd(Dd18O.first.differences., na.rm=T),
-                        Dd18O.first.differences.ABSMean=mean(abs(Dd18O.first.differences.), na.rm=T),
-                        Dd18O.first.differences.ABSMeanWeighted = weighted.mean(abs(Dd18O.first.differences.), Length, na.rm=T),
-                        d18O.meanObserved =mean(d18O.mean ),
-                        d18O.meanObservedWeighted = weighted.mean(d18O.mean, Length),
-                        d18O.meanObservedSD = sd(d18O.mean),
-                        d18O.CV = sd(d18O.mean)/ mean(d18O.mean)
-                        )})
+#   vez<-with(veizer[veizerIDX,], {
+#                       c(d18OresidualMean=mean(d18Oresidual),
+#                         d18OresidualMeanWeighted=weighted.mean(d18Oresidual, Length),
+#                         d18OresidualSD=sd(d18Oresidual),
+#                         Dd18O.first.differences.Mean=mean(Dd18O.first.differences., na.rm=T),
+#                       #  Dd18O.first.differences.MeanWeighted=weighted.mean(Dd18O.first.differences., length, na.rm=T),
+#                         Dd18O.first.differences.SD=sd(Dd18O.first.differences., na.rm=T),
+#                         Dd18O.first.differences.ABSMean=mean(abs(Dd18O.first.differences.), na.rm=T),
+#                         Dd18O.first.differences.ABSMeanWeighted = weighted.mean(abs(Dd18O.first.differences.), Length, na.rm=T),
+#                         d18O.meanObserved =mean(d18O.mean ),
+#                         d18O.meanObservedWeighted = weighted.mean(d18O.mean, Length),
+#                         d18O.meanObservedSD = sd(d18O.mean),
+#                         d18O.CV = sd(d18O.mean)/ mean(d18O.mean)
+#                         )})
 
     sea <- with(sealevel[sealevelIDX,], {
     	c(sea_level...first.diffs. = mean(sea_level...first.diffs.),
@@ -240,15 +253,17 @@ envtCols <- t(apply(ext[,stageIDX], 1, function(arow){
     )
 
 
-  ret<-c(times, vb, ex, envt, vez, ex2, sea)
+  ret<-c(times, vb, ex, envt, ex2, sea)
   names(ret)<-names(ret)
-
+  colNames <<- c(names(times), names(vb), names(ex), names(envt), names(ex2), names(sea))
+  
   if(flag==1) ret<-rep(NA, length(ret))
 
   return(ret)
 }))
 
 envtCols<-as.data.frame(envtCols)
+names(envtCols) <- colNames
 
 #debug
 #plot(del.18O ~ d18O.observations, data= envtCols)
@@ -351,23 +366,22 @@ for(i in 2:nrow(ext)){
 #ext[checkme,]
 
 # bring in new d18O and d13C data:
-gr <- read.csv("../data/Grossman_d18O.d13C_with_high.lat.csv")
-gr$Binned_top <- round(gr$Binned_top, 2)
-ext$Binned_top <- round(ext$startTime.Ma, 2)
-d18Odat <- gr[gr$data_subset == "All" & gr$Binned_top < 500,c("Binned_top", "mean_d18O")]
-d13Cdat <- gr[gr$data_subset == "All" & gr$Binned_top < 500,c("Binned_top", "mean_d13C")]
-d18Odat$mean_d18O <- with(d18Odat, detrend_ts(Binned_top, mean_d18O, "Grossman d18O (All)"))
-d13Cdat$mean_d13C <- with(d13Cdat, detrend_ts(Binned_top, mean_d13C, "Grossman d13C (All)"))
-dev.off()
+# gr <- read.csv("../data/Grossman_d18O.d13C_with_high.lat.csv")
+# gr$Binned_top <- round(gr$Binned_top, 2)
+# ext$Binned_top <- round(ext$startTime.Ma, 2)
+# d18Odat <- gr[gr$data_subset == "All" & gr$Binned_top < 500,c("Binned_top", "mean_d18O")]
+# d13Cdat <- gr[gr$data_subset == "All" & gr$Binned_top < 500,c("Binned_top", "mean_d13C")]
+# d18Odat$mean_d18O <- with(d18Odat, detrend_ts(Binned_top, mean_d18O, "Grossman d18O (All)"))
+# d13Cdat$mean_d13C <- with(d13Cdat, detrend_ts(Binned_top, mean_d13C, "Grossman d13C (All)"))
 
-ext <- plyr::join(ext, d18Odat)
-ext <- plyr::join(ext, d13Cdat)
 
-ext$del.18O <- ext$mean_d18O
-ext$del.13C <- ext$mean_d13C
-ext$mean_d18O <- NULL # clean up
-ext$mean_d13C <- NULL # clean up
+# ext <- plyr::join(ext, d18Odat)
+# ext <- plyr::join(ext, d13Cdat)
 
+# ext$del.18O <- ext$mean_d18O
+# ext$del.13C <- ext$mean_d13C
+# ext$mean_d18O <- NULL # clean up
+# ext$mean_d13C <- NULL # clean up
 
 
 ###################################
