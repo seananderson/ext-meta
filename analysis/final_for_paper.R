d0b421b2671e87d12d11a8ecdc9a3b185327604e
Jarrett Byrnes <jarrett.byrnes@umb.edu>
2013-12-28 21:13:08 -0500

    Run final analyses with Prokoph proxies & centering

--- analysis/final_for_paper.R
+++ analysis/final_for_paper.R
@@ -5,11 +5,13 @@
 ## from NESCent Working Group for publication
 ##
 ## Created:       Jan 13, 2012
-## Last modified: Oct 31, 2013
+## Last modified: Dec 28, 2013
 ## Purpose:       Try plotting the effect sizes against the raw data.
 ## Additional description: More analyses can be found in singleLnOr_R_analyses/singleLnOr_rma.R
 ## Changelog
 ##
+## Dec 28,2013 - Added xAdd argumen to marginal plotting to allow for adjusting for 
+##                offsets from centering predictors
 ##################################
 
 #####
@@ -39,6 +41,13 @@ gastropodsBroadData<-subset(broadData, broadData[["Bivalve..Gastropod"]]!="Gastr
 bivalveBroadData$Tax.level<-factor(bivalveBroadData$Tax.level)
 gastropodsBroadData$Tax.level<-factor(gastropodsBroadData$Tax.level)
 
+#centering Function
+cent <- function (x) x-mean(x, na.rm=T)
+
+
+
+
+
 ## First, let's look at the broadly distributed data
 ## and the difference between broad and narro
 ## So, subset the data down to broad being the grouping trait of interest
@@ -55,6 +64,7 @@ habitData$Tax.level<-factor(habitData$Tax.level)
 habitData<-habitData[which(habitData$vlnorReg>0),]
 
 habitData$study.ID <- factor(habitData$study.ID)
+
 
 
 ########################################
@@ -138,22 +148,31 @@ broad.rma
 ##########
 ## @knitr bigBroadModelRMA
 broadDataExtinction <- broadData[which(!is.na(broadData$BC.extinction.ratePBDB)),]
-broadDataExtinction <- broadDataExtinction[which(!is.na(broadDataExtinction$del.18O)),]
-broadDataExtinction <- broadDataExtinction[which(!is.na(broadDataExtinction$del.34S)),]
-broadDataExtinction <- broadDataExtinction[which(!is.na(broadDataExtinction$del.13C)),]
+broadDataExtinction <- broadDataExtinction[which(!is.na(broadDataExtinction$mean_d18O.prok)),]
+broadDataExtinction <- broadDataExtinction[which(!is.na(broadDataExtinction$mean_d34S.prok)),]
+broadDataExtinction <- broadDataExtinction[which(!is.na(broadDataExtinction$mean_d13C.prok)),]
 
-covModel.Broad.RMA <-rma(yi = lnorReg, vi = vlnorReg, data=broadDataExtinction, mods=~OA+ BC.extinction.ratePBDB +
-                           del.18O + del.34S +del.13C)
+#as we'll be using these predictors later
+broadDataExtinction <- within(broadDataExtinction, {
+  cent.extinction <- cent(BC.extinction.ratePBDB)
+  cent.OA <- cent(OA)
+  cent.d18O <- cent(mean_d18O.prok)
+  cent.d34S <- cent(mean_d34S.prok)
+  cent.d13C <- cent(mean_d13C.prok)
+})
+
+covModel.Broad.RMA <- rma(yi = lnorReg, vi = vlnorReg, data=broadDataExtinction, mods=~BC.extinction.ratePBDB +
+      cent.OA + cent.d18O + cent.d34S + cent.d13C)
 
 covModel.Broad.RMA
 
 write.csv(coef(covModel.Broad.RMA), "./broadCoefTable.csv", row.names=T)
 
 broadCoefPlot <- coefPlot(covModel.Broad.RMA, robust=F, std=T)+
-  scale_x_discrete(labels=c("Extinction Rate", expression(delta^18*O), expression(delta^13*C), expression(delta^34*S), "Acidification"), expand = c(0.15, 0)) +
-  annotate("text", x=5, y=-0.4, label="A)")+
-  ylim(c(-0.5,0.5)) +
   coord_flip() +
+  scale_x_discrete(labels=c("Extinction Rate", expression(delta^13*C), expression(delta^18*O), expression(delta^34*S), "Acidification"), expand = c(0.15, 0)) +
+  annotate("text", x=5, y=-0.5, label="A)")+
+  ylim(c(-0.55,0.4)) +
   annotate("text", x=5.6, y=-0.35, label="Favours\nnarrow")+
   annotate("text", x=5.6, y=0.35, label="Favours\nbroad")
 
@@ -193,24 +212,37 @@ meanModel.Epifaunal
 ## @knitr epibigEpifaunaModel.RMA
 habitDataGood <- habitData[which(!(is.na(habitData$BC.extinction.ratePBDB))),]
 habitDataGood <- habitDataGood[which(!(is.na(habitDataGood$lnorReg))),]
-habitDataGood <- habitDataGood[which(!(is.na(habitDataGood$del.34S))),]
-habitDataGood <- habitDataGood[which(!(is.na(habitDataGood$del.18O))),]
+habitDataGood <- habitDataGood[which(!(is.na(habitDataGood$mean_d18O.prok))),]
+habitDataGood <- habitDataGood[which(!(is.na(habitDataGood$mean_d34S.prok))),]
+habitDataGood <- habitDataGood[which(!(is.na(habitDataGood$vlnorReg))),]
+habitDataGood <- habitDataGood[which(!(is.na(habitDataGood$BC.extinction.ratePBDB))),]
+
+
+#as we'll be using these predictors later
+habitDataGood <- within(habitDataGood, {
+  cent.extinction <- cent(BC.extinction.ratePBDB)
+  cent.OA <- cent(OA)
+  cent.d18O <- cent(mean_d18O.prok)
+  cent.d34S <- cent(mean_d34S.prok)
+})
 
 
 # TODO WARNING Error in qr.solve(wX, diag(k)) : singular matrix 'a' in solve
 # TODO WARNING there are no OA events: they're all 0
+
 covModel.Epifaunal.rma <-rma(yi = lnorReg, vi = vlnorReg, data=habitDataGood,
-                             mods =~  OA + BC.extinction.ratePBDB + del.18O + del.34S)
+                              mods =~ cent.OA + cent.extinction + cent.d18O + cent.d34S)
+
 
 covModel.Epifaunal.rma
 write.csv(coef(covModel.Epifaunal.rma), "./epiCoefTable.csv", row.names=T)
 
 
 epiCoefPlot <- coefPlot(covModel.Epifaunal.rma, habitDataGood, robust=F, std=T)+
-  scale_x_discrete(labels=c("Extinction Rate",  expression(delta^18*O), expression(delta^34*S), "Acidification"), expand = c(0.15, 0)) +
-  annotate("text", x=4, y=-1.0, label="B)")+
-  ylim(c(-1.25,1.25)) +
   coord_flip() +
+  scale_x_discrete(labels=c(expression(delta^18*O), expression(delta^34*S), "Extinction Rate", "Acidification"), expand = c(0.15, 0)) +
+  annotate("text", x=4, y=-1.0, label="B)")+
+  ylim(c(-1.3,1.3)) +
   annotate("text", x=4.6, y=-.7, label="Favours\ninfauna")+
   annotate("text", x=4.6, y=.7, label="Favours\nepifauna")
 
@@ -220,21 +252,24 @@ grid.arrange(broadCoefPlot+theme_bw(base_size=18), epiCoefPlot+theme_bw(base_siz
 ## @knitr Fig5
 
 ####What are the marginal effects from the model
-del18marg <- marginalLine(covModel.Epifaunal.rma, "del.18O", habitDataGood, robust=F)+
-  xlab("\n Detrended Delta O18") +
-  ylab("Component + Residual + Intercept Log Odds\n Ratios for Detrended Delta O18\n") +
-  annotate("text", x=-4, y=8.75, label="A)") + scale_color_discrete(guide="none") +
+####After adjusting for centering the predictor
+del18marg <- marginalLine(covModel.Epifaunal.rma, "cent.d18O", 
+                          habitDataGood, robust=F, xAdd=mean(habitDataGood$mean_d18O.prok))+
+  xlab("\nDelta O18") +
+  ylab("Component + Residual + Intercept Log Odds\n Ratios for Delta O18\n") +
+  annotate("text", x=-0.6, y=2.75, label="A)") + scale_color_discrete(guide="none") +
   theme_bw(base_size=18)
 
-del18MargData<- marginalData(covModel.Epifaunal.rma, "del.18O", habitDataGood)
+del18MargData<- marginalData(covModel.Epifaunal.rma, "cent.d18O", habitDataGood)
 write.csv(del18MargData, "./del18MargData.csv", row.names=F)
 
-del34marg <- marginalLine(covModel.Epifaunal.rma, "del.34S", habitDataGood, robust=F) +
-  xlab("\n Delta S34") + ylab("Component + Residual + Intercept Log Odds\n Ratios for Delta 34S\n") +
-  annotate("text", x=13.75, y=3.375, label="B)")+
+del34marg <- marginalLine(covModel.Epifaunal.rma, "cent.d34S", 
+                          habitDataGood, robust=F,  xAdd=mean(habitDataGood$mean_d34S.prok)) +
+  xlab("\nDelta S34") + ylab("Component + Residual + Intercept Log Odds\n Ratios for Delta 34S\n") +
+  annotate("text", x=18.75, y=3.375, label="B)")+
   theme_bw(base_size=18)
 
-del34margData<- marginalData(covModel.Epifaunal.rma, "del.34S", habitDataGood)
+del34margData<- marginalData(covModel.Epifaunal.rma, "cent.d34S", habitDataGood)
 write.csv(del34margData, "./del34margData.csv", row.names=F)
 
 #Extract Legend
@@ -261,9 +296,7 @@ jackknifed_coefs_fun(covModel.Broad.RMA, broadDataExtinction, robust=F) + theme_
   scale_colour_grey(name="Study Removed\n")
 
 
-# TODO WARNING
-# Error in rma(lnorReg, vi = vlnorReg, data = temp_dat, mods = temp_dat[,  :
-# Processing terminated since k = 0.
+# What is going on with that ONE point?
 jackknifed_coefs_fun(covModel.Epifaunal.rma, habitDataGood, robust=F) +theme_bw()+
   scale_colour_grey(name="Study Removed\n")
 
