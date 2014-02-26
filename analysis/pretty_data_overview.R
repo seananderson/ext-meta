4f3018bed5a3db6631ddac6543d80f9c3c407ce2
Sean Anderson <github@seananderson.ca>
2014-02-26 13:21:11 -0800

    Adapt data overview fig to new proxy data

--- analysis/pretty_data_overview.R
+++ analysis/pretty_data_overview.R
@@ -3,7 +3,7 @@
 #Figure 2: Sean's summary graph of (A) log-adds geo range (i.e., GSA slide 10), (2) log-odds life habit (i.e., GSA slide 14), (3) ext rates, (4) del18O residuals, (5) del13C, (6) del34S, (7) OAE, and (8) predation through time
 
 
-d18dat <- gr[gr$data_subset == "All" ,c("Binned_Age", "mean_d18O")]
+#d18dat <- temp[gr$data_subset == "All" ,c("Binned_Age", "mean_d18O")]
 
 #####
 source("../r/data_clean_merge.r")
@@ -11,11 +11,12 @@ source("../r/metaprep.r")
 source("../r/lmerMetaPrep.R")
 # sealevel : Time..my. and del.34S and del.18O
 # extMag has BC.extinction.rate and Midpoint..Ma.
+stageTime <- transform(stageTime, Midpt = Start..Ma. - (Start..Ma. - End..Ma.)/2)
 
 # volcbolide has State.stage and OA, will need to merge with
 # stageTime[,c("Bin.name", "Start..Ma.", "Midpt", "End..Ma.")]
-names(stageTime)[2] <- "State.stage"
-volcbolide2 <- merge(volcbolide, stageTime[,c("State.stage", "Start..Ma.", "Midpt", "End..Ma.")], all.x = TRUE) 
+#names(stageTime)[2] <- "State.stage"
+#volcbolide2 <- merge(volcbolide, stageTime[,c("State.stage", "Start..Ma.", "Midpt", "End..Ma.")], all.x = TRUE)
 
 # get ORs in a nice format for plotting:
 broad_d <- subset(ext, Aggregate.Trait == "Geographic Range" & Trait.category == "Broad")[,c("study.ID", "lnorReg", "vlnorReg", "meanDate")]
@@ -23,7 +24,7 @@ broad_d$effect_type <- "range"
 broad_d$study.ID <- as.character(broad_d$study.ID)
 broad_d$study_num <- as.numeric(as.factor(broad_d$study.ID))
 
-epi_d <- subset(ext, Aggregate.Trait == "Life Habit" & Trait.category %in% c("Epifaunal", "Epifauna", "Epifuanl", "Epifaunalcalciticouterlayer", "Epifaunalcompetelyaragonitic"))[,c("study.ID", "lnorReg", "vlnorReg", "meanDate")] 
+epi_d <- subset(ext, Aggregate.Trait == "Life Habit" & Trait.category %in% c("Epifaunal", "Epifauna", "Epifuanl", "Epifaunalcalciticouterlayer", "Epifaunalcompetelyaragonitic"))[,c("study.ID", "lnorReg", "vlnorReg", "meanDate")]
 epi_d$effect_type <- "habit"
 epi_d$study.ID <- as.character(epi_d$study.ID)
 epi_d$study_num <- as.numeric(as.factor(epi_d$study.ID))
@@ -54,7 +55,7 @@ par(xpd = NA)
 line_col <- "#00000090"
 ylabel <- function(text, line = -1.3) mtext(text, side = 3, cex = 0.8, line = line, col = "grey50", adj = 0)
 ann <- function(x, y, add.last = TRUE) {
-  
+
   x <- x[!is.na(y)]
   y <- y[!is.na(y)]
 
@@ -69,10 +70,10 @@ ann <- function(x, y, add.last = TRUE) {
   x.last <- x[last.pos]
   y.last <- y[last.pos]
 
-  text(x.min, y.min, sprintf("%.1f", round(y.min, 1)), col = "grey50", pos = 1, cex = 0.8, offset = 0.25) 
-  text(x.max, y.max, sprintf("%.1f", round(y.max, 1)), col = "grey50", pos = 3, cex = 0.8, offset = 0.25) 
+  text(x.min, y.min, sprintf("%.1f", round(y.min, 1)), col = "grey50", pos = 1, cex = 0.8, offset = 0.25)
+  text(x.max, y.max, sprintf("%.1f", round(y.max, 1)), col = "grey50", pos = 3, cex = 0.8, offset = 0.25)
   if(add.last)
-    text(x.last, y.last, sprintf("%.1f", round(y.last, 1)), col = "grey50", pos = 3, cex = 0.8, offset = 0.25) 
+    text(x.last, y.last, sprintf("%.1f", round(y.last, 1)), col = "grey50", pos = 3, cex = 0.8, offset = 0.25)
 }
 
 
@@ -105,36 +106,36 @@ col.df <- effect_d
 col.df <- col.df[!duplicated(col.df[,c("study.ID", "study_col", "effect_type")]), ]
 #with(subset(col.df, effect_type == "range"), legend(x = 400, y = 4, legend = study.ID, col = "#00000030", pt.bg = study_col, bty = "n", pch = 21))
 
-with(volcbolide2, plot(Midpt, rep(1, nrow(volcbolide2)), pch = c(21, 19)[OA+1], xlim = xlim, axes = F, xlab = "", col = c("#00000020", "#00000080")[OA+1], cex = 1.5, ylab = ""))
+with(stageTime, plot(Midpt, rep(1, nrow(stageTime)), pch = c(21, 19)[OA+1], xlim = xlim, axes = FALSE, xlab = "", col = c("#00000020", "#00000080")[OA+1], cex = 1.5, ylab = ""))
 ylabel(expression((c)~OA~Event))
 
-with(d18Odat, plot(Binned_Age, mean_d18O, type = "l", xlim = xlim, axes = F, xlab = "", col = line_col, ylab = ""))
+with(subset(stageTime, !is.na(mean_d18O.prok)), plot(Midpt, mean_d18O.prok, type = "l", xlim = xlim, axes = FALSE, xlab = "", col = line_col, ylab = ""))
 ylabel(expression((d)~delta^18*O~residual))
 #ylabel("residuals")
-with(d18Odat, ann(Binned_Age, mean_d18O))
+with(stageTime, ann(Midpt, mean_d18O.prok))
 
 
-with(d13Cdat, plot(Binned_Age, mean_d13C, type = "l", xlim = xlim, axes = F, xlab = "", col = line_col, ylab = ""))
+with(subset(stageTime, !is.na(mean_d13C.prok)), plot(Midpt, mean_d13C.prok, type = "l", xlim = xlim, axes = FALSE, xlab = "", col = line_col, ylab = ""))
 ylabel(expression((e)~delta^13*C~residual))
-with(d13Cdat, ann(Binned_Age, mean_d13C))
+with(stageTime, ann(Midpt, mean_d13C.prok))
 
-with(na.omit(proxy[,c("top", "del.34S")]), plot(top, del.34S, type = "l", xlim = xlim, axes = F, xlab = "", col = line_col, ylab = ""))
+with(subset(stageTime, !is.na(mean_d34S.prok)), plot(Midpt, mean_d34S.prok, type = "l", xlim = xlim, axes = FALSE, xlab = "", col = line_col, ylab = ""))
 ylabel(expression((f)~delta^34*S~residual))
-with(proxy, ann(top, del.34S))
+with(stageTime, ann(Midpt, mean_d34S.prok))
 
 
-with(extMag, plot(Midpoint..Ma., BC.extinction.rate, xlim = xlim, axes = F, type = "l", xlab = "", col = line_col, ylab = ""))
+with(stageTime, plot(Midpt, BC.extinction.ratePBDB, xlim = xlim, axes = FALSE, type = "l", xlab = "", col = line_col, ylab = ""))
 ylabel(expression((g)~Extinction~rate))
-with(extMag, ann(Midpoint..Ma., BC.extinction.rate, add.last = FALSE))
+with(stageTime, ann(Midpt, BC.extinction.ratePBDB, add.last = FALSE))
 
 #with(stageTime, segments(Start..Ma., rep(0, nrow(stageTime)), Start..Ma., rep(0.7, nrow(stageTime)), col = "#00000017"))
 
-axis(1, col.axis = "grey50", col = "grey50", tck = -0.09, padj = -1.2, cex.axis = 1.1, line = 0.95)
+axis(1, col.axis = "grey50", col = "grey50", tck = -0.09, padj = -1.2, cex.axis = 1.1, line = 1.15)
 mtext("Geologic time (Ma)", side = 1, line = 2.7, col = "grey50", cex = 0.8)
 #axis(2)
 
 source("time_scale.R") # based on Carl's function; add rects()
-tscales(top = -0.01, s.bot = -0.25, reso = "periods", cex = 0.9, lwd = 1)
+tscales(top = -0.01, s.bot = -0.22, reso = "periods", cex = 0.9, lwd = 1)
 
 dev.off()
 
